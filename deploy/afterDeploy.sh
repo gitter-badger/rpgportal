f2ab47fd96376cd746e013e6fd4bd4151fde31ac
deathman92 <deathman1912@gmail.com>
2016-03-27 02:37:49 +0300

    Update after deploy script

--- deploy/afterDeploy.sh
+++ deploy/afterDeploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd boxfuse
-logfile = '*.logs'
-logs = `cat $logfile`
-deployed = false
+logfile='*.logs'
+logs=$(cat $logfile)
+deployed=false
 echo Parse logs
 for log in $logs ; do
     if [[ $log == *"Deployment completed successfully"* ]]
     then
-        deployed = true
+        deployed=true
     fi
 done
 if [[ $deployed == false ]]
