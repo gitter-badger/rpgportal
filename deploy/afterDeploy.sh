09c10afcb043ed0035636b376520b16bb5b7afea
deathman92 <deathman1912@gmail.com>
2016-03-27 15:18:22 +0300

    Update after deploy script

--- deploy/afterDeploy.sh
+++ deploy/afterDeploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+chmod +x deploy/removeImage.sh
 echo Check deployed...
 deployed=true
-./removeImage.sh rpgportal $TRAVIS_BUILD_NUMBER && deployed=false
+./deploy/removeImage.sh rpgportal $TRAVIS_BUILD_NUMBER && deployed=false
 if [ $deployed ]
 then
     echo Not deployed! Removing current image...
-    ./removeImage.sh rpgportal $TRAVIS_BUILD_NUMBER -v && echo Successfully removed rpgportal:$TRAVIS_BUILD_NUMBER.
+    ./deploy/removeImage.sh rpgportal $TRAVIS_BUILD_NUMBER -v && echo Successfully removed rpgportal:$TRAVIS_BUILD_NUMBER.
     echo -e "\nDone. Your build exited with 1." && exit 1;
 fi
 prevNum=`expr $TRAVIS_BUILD_NUMBER - 1`
 echo Deployed. Removing prevoius image rpgportal:$prevNum...
-./removeImage.sh rpgportal $prevNum -v && echo Successfully removed rpgportal:$prevNum. || echo Cannot remove previous image! Do it manually.
+./deploy/removeImage.sh rpgportal $prevNum -v && echo Successfully removed rpgportal:$prevNum. || echo Cannot remove previous image! Do it manually.
 exit 0;
