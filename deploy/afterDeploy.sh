4150e2a12a9a5691d418e6718450022f61404a85
deathman92 <deathman1912@gmail.com>
2016-03-27 04:53:25 +0300

    Update after deploy script

--- deploy/afterDeploy.sh
+++ deploy/afterDeploy.sh
@@ -6,9 +6,10 @@ if [ $deployed == false ]
 then
     echo Not deployed! Removing current image
     boxfuse/boxfuse rm rpgportal:$TRAVIS_BUILD_NUMBER -vault && echo Removed. Exit
-    travis_terminate 1;
+    exit 1;
 fi
 prevNum=`expr $TRAVIS_BUILD_NUMBER - 1`
 echo Deployed. Removing prevoius image num.$prevNum
-boxfuse/boxfuse rm rpgportal:$prevNum -vault && echo Deployed!
+boxfuse/boxfuse rm rpgportal:$prevNum -vault && echo Deployed! && exit 0;
 echo Cannot remove previous image!
+exit 0;
