ee589784697bce1120d70b79a1b029dd9de2e475
deathman92 <deathman1912@gmail.com>
2016-03-27 04:38:52 +0300

    Update after deploy script

--- deploy/afterDeploy.sh
+++ deploy/afterDeploy.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-url=http://rpgportal-test-deathman92.boxfuse.io/health
-echo Check deploy by url $url
-r=`wget -q $url`
-if [ $? != 0 ]
-then
-    deployed=false
-else
-    deployed=true
-fi
+echo Check deployed
+deployed=true
+boxfuse/boxfuse rm rpgportal:$TRAVIS_BUILD_NUMBER && deployed=false
 if [ $deployed == false ]
 then
     echo Not deployed! Removing current image
     boxfuse/boxfuse rm rpgportal:$TRAVIS_BUILD_NUMBER -vault && echo Removed. Exit
-    travis_terminate 1
-    exit 1
+    travis_terminate 1;
 fi
 prevNum=`expr $TRAVIS_BUILD_NUMBER - 1`
 echo Deployed. Removing prevoius image num.$prevNum
